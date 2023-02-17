fced4c794905e2da7373bdd840c1bc6f6b3d07d4
Yann Cabral <iamyanndias@gmail.com>
2023-02-17 10:06:20 -0300

    Add interface to http client.

---
+++ lib/src/infra/repository_http_client.dart
@@ -0,0 +1,9 @@
+/// {@template repository_http_client}
+/// Abstract class for HTTP client to be used in http `Repositores`.
+/// You can use this class to create your own HTTP client,
+/// or just use the default one.
+/// {@endtemplate}
+abstract class RepositoryHttpClient {
+  /// {@macro repository_http_client}
+  const RepositoryHttpClient();
+}
