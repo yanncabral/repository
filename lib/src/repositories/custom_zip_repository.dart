5639d04133c148c5e4c95be92c56e78a08f7542b
Yann Cabral <iamyanndias@gmail.com>
2023-02-22 14:04:40 -0300

    Fix zip repository.

--- lib/src/repositories/custom_zip_repository.dart
+++ lib/src/repositories/custom_zip_repository.dart
@@ -1,8 +1,15 @@
+import 'package:repository/src/repository.dart';
 import 'package:rxdart/streams.dart';
 
-import '../repository.dart';
-
+/// {@template custom_zip_repository}
+/// A [Repository] that combines multiple [Repository]s into one.
+/// It takes a list of data of each repository and returns a single zipped data.
+/// It is useful when you want to combine multiple repositories into one.
+/// {@endtemplate}
 abstract class CustomZipRepository<Data> extends Repository<Data> {
+  /// {@macro custom_zip_repository}
+  CustomZipRepository({super.autoRefreshInterval, super.resolveOnCreate});
+
   @override
   String get key => repositories.map((e) => e.key).join('-');
 
@@ -23,11 +30,8 @@ abstract class CustomZipRepository<Data> extends Repository<Data> {
       ZipStream(repositories.map((e) => e.stream), zipper);
 
   /// A list of repositories that will be zipped.
-  List<Repository> get repositories;
+  List<Repository<dynamic>> get repositories;
 
   /// Takes a list of data of each repository and returns a single zipped data.
   Data zipper(List<dynamic> values);
-
-  /// Creates a [CustomZipRepository] that combines multiple [Repository]s into one.
-  CustomZipRepository({super.autoRefreshInterval, super.resolveOnCreate});
 }
--- lib/src/repositories/zip_repository.dart
+++ lib/src/repositories/zip_repository.dart
@@ -2,34 +2,38 @@
 
 // import 'custom_zip_repository.dart';
 
-// /// A [Repository] that combines multiple [Repository]s into one.
-// /// It takes a list of [Repository]s and a [zipper] function.
-// /// The [zipper] function takes a list of streams and returns a single stream.
-// /// The [zipper] function is called every time one of the [Repository]s emits a
-// /// new value.
-// class ZipRepository<Data> extends CustomZipRepository<Data> {
-//   /// The list of [Repository]s to combine.
-//   /// The [zipper] function will be called every time one of the [Repository]s
-//   /// emits a new value.
-//   @override
-//   final List<Repository> repositories;
+import 'package:repository/src/repositories/custom_zip_repository.dart';
+import 'package:repository/src/repository.dart';
 
-//   /// The function that combines the streams of the [Repository]s into one.
-//   final Data Function(List<dynamic> streams) _zipper;
+/// {@template zip_repository}
+/// Creates a [ZipRepository] that combines multiple [Repository]s into one.
+/// It takes a list of [Repository]s and a [zipper] function.
+/// The [zipper] function takes a list of streams and returns a single stream.
+/// The [zipper] function is called every time one of the [Repository]s emits a
+/// new value.
+/// {@endtemplate}
+class ZipRepository<Data> extends CustomZipRepository<Data> {
+  /// {@macro zip_repository}
+  ZipRepository({
+    required this.repositories,
+    required Data Function(List<dynamic> streams) zipper,
+    super.autoRefreshInterval,
+    super.resolveOnCreate,
+  }) : _zipper = zipper;
 
-//   /// Creates a [ZipRepository] that combines multiple [Repository]s into one.
-//   /// It takes a list of [Repository]s and a [zipper] function.
-//   /// The [zipper] function takes a list of streams and returns a single stream.
-//   /// The [zipper] function is called every time one of the [Repository]s emits a
-//   /// new value.
-//   ZipRepository({
-//     required this.repositories,
-//     required Data Function(List<dynamic> streams) zipper,
-//     super.autoRefreshInterval,
-//     super.resolveOnCreate,
-//   }) : _zipper = zipper;
+  /// The list of [Repository]s to combine.
+  /// The [zipper] function will be called every time one of the [Repository]s
+  /// emits a new value.
+  @override
+  final List<Repository<dynamic>> repositories;
 
-//   /// This function is called on resolve to get the streams to combine.
-//   @override
-//   Data zipper(List<dynamic> values) => _zipper(values);
-// }
+  /// The function that combines the streams of the [Repository]s into one.
+  final Data Function(List<dynamic> streams) _zipper;
+
+  /// This function is called on resolve to get the streams to combine.
+  @override
+  Data zipper(List<dynamic> values) => _zipper(values);
+
+  @override
+  String? get tag => repositories.map((e) => e.tag).join('-');
+}
