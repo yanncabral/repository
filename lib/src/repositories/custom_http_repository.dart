a3489df48768a0d330ae928bc21c679ed557d8e7
Yann <iamyanndias@gmail.com>
2024-02-21 03:33:21 -0300

    Refactor CustomHttpRepository class

--- lib/src/repositories/custom_http_repository.dart
+++ lib/src/repositories/custom_http_repository.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:http/http.dart';
 import 'package:repository/src/domain/exceptions/unexpected_status_code_exception.dart';
 import 'package:repository/src/external/http_repository_http_client.dart';
 import 'package:repository/src/infra/repository_http_client.dart';
 import 'package:repository/src/infra/repository_logger.dart';
 import 'package:repository/src/repository.dart';
+
+/// A type alias for bearer token String.
+/// Notice that this is just the token, without the `Bearer` prefix.
+typedef RepositorySession = ({String userId, String bearerToken});
 
 /// {@template custom_http_repository}
 /// A [CustomHttpRepository] is a [Repository] that fetches data from an Http
@@ -17,22 +22,17 @@ abstract class CustomHttpRepository<Data> extends Repository<Data> {
   CustomHttpRepository({
     super.autoRefreshInterval,
     super.resolveOnCreate,
-    this.tag,
     this.accessTokenBuilder,
-  });
+    this.headers,
+    this.queryParameters,
+    RepositoryHttpClient? client,
+  }) : client = client ?? HttpRepositoryHttpClient();
 
-  /// The access token builder. It'll be sent as a Bearer token in the
-  /// Authorization header if it's not null.
-  Future<String> Function()? accessTokenBuilder;
-
-  /// The tag of the repository.
-  /// This is used to identify the repository in the cache.
-  /// If the tag is not null, the repository will use the tag to
-  /// create unique keys for the cache.
-  /// A commom use case for this is when you want to cache for different
-  /// users. In this case, you can use the user id (e.g. e-mail) as the tag.
   @override
-  final String? tag;
+  String get key => endpoint.toString();
+
+  /// A function that returns the access token to be used in the request.
+  Future<String> Function()? accessTokenBuilder;
 
   /// If the request fails with [SocketException], this callback will be called
   /// with the repository itself as an argument.
@@ -44,25 +44,45 @@ abstract class CustomHttpRepository<Data> extends Repository<Data> {
   /// The endpoint to fetch data from.
   Uri get endpoint;
 
-  // Dependencies
+  /// The headers to be used in the request.
+  final Map<String, String>? headers;
+
+  /// The query parameters to be used in the request.
+  Map<String, dynamic>? queryParameters;
+
+  /// Sets the query parameters to be used in the request.
+  void setQueryParameters(Map<String, dynamic> params) {
+    queryParameters = params;
+    refresh();
+  }
 
   /// The http client used to fetch data from the endpoint.
-  /// This is a monostate, so it will be shared across all instances of
-  /// [CustomHttpRepository] and `HttpRepository`.
-  static final client = HttpRepositoryHttpClient();
+  final RepositoryHttpClient client;
 
   @override
-  Future<String> resolve() async {
+  Future<({String body, String? tag})> resolve() async {
     try {
-      final request = RepositoryHttpRequest(url: endpoint);
+      final session = await CustomHttpRepository.sessionBuilder?.call();
+
+      final url = queryParameters != null
+          ? endpoint.replace(queryParameters: queryParameters)
+          : endpoint;
+
+      final request = RepositoryHttpRequest(
+        url: url,
+        headers: {
+          if (session case RepositorySession(:final String bearerToken))
+            'Authorization': bearerToken,
+          ...?headers,
+        },
+      );
+
       final response = await client.get(request: request);
 
       /// If the endpoint returns a 200, add the data to the stream
       /// and cache it
       if (successfulCondition(response.statusCode, response.body)) {
-        final result = response.body;
-
-        return result;
+        return (body: response.body, tag: session?.userId);
       } else {
         Repository.logger(
           'Repository($name): Failed to resolve [$endpoint]. '
@@ -84,7 +104,7 @@ abstract class CustomHttpRepository<Data> extends Repository<Data> {
         level: RepositoryLoggingLevel.warning,
       );
 
-      if (exception is Exception) {
+      if (exception is SocketException) {
         await onSocketException(exception);
       }
 
@@ -93,13 +113,13 @@ abstract class CustomHttpRepository<Data> extends Repository<Data> {
   }
 
   @override
-  String get name => endpoint.path.split('/').last;
+  String? get name => endpoint.path.split('/').last;
 
   /// Condition to determine if the endpoint returns a successful status code.
   /// The default condition is `statusCode >= 200 && statusCode < 300`.
   /// If you want to change the condition, override this method.
   bool successfulCondition(int statusCode, dynamic body) {
-    return statusCode == 200 || statusCode == 201;
+    return statusCode >= 200 && statusCode < 300;
   }
 
   /// Called when the endpoint returns a unsuccessful status code.
@@ -107,19 +127,12 @@ abstract class CustomHttpRepository<Data> extends Repository<Data> {
   Future<void> onErrorStatusCode(int statusCode) async {}
 
   @override
-  String get key {
-    /// We combine the `tag` and the `endpoint` to create a unique key
-    /// and we use the md5 hash to create a unique but "shorter" key.
-    /// When the `tag` is null, we use only the `endpoint` as the key.
-
-    if (tag == null) {
-      return endpoint.toString();
-    } else {
-      return '$tag,$endpoint';
-    }
-  }
-
-  @override
   FutureOr<bool> shouldRetry(Exception exception) =>
-      exception is SocketException;
+      exception is SocketException || exception is ClientException;
+
+  /// Sets the [sessionBuilder] to be used by all [CustomHttpRepository].
+  static SessionBuilder? sessionBuilder;
 }
+
+/// A type alias for a function that returns a [RepositorySession].
+typedef SessionBuilder = Future<RepositorySession?> Function();
